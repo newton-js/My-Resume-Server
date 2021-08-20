3d0cbde2c3fbc14b7ed1855187a65719bd2ec852
MAC <mac@MACs-MacBook-Pro.local>
2021-08-20 09:25:05 +0100

    added print-name route

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ var port = process.env.PORT || 3000
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'resumeview')))
+app.use(express.static(path.join(__dirname, 'printNameView')))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'resumeview', 'resumeHTML.html'))
+})
+
+app.get('/print-name', (req, res) => {
+    res.sendFile(path.join(__dirname, 'printNameView', 'printHTML.html'))
 })
 
 app.listen(port, () => {
